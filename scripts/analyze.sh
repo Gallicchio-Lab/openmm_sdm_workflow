b17452ba196844f422e54ddde663b60a49452d6b
Emilio Gallicchio <egallicchio@brooklyn.cuny.edu>
2019-04-19 09:05:24 -0400

    discard samples not cycles

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-set_basename=t4l
-work_dir=/home/emilio/Dropbox/sims/bind_phaset/t4l
-scripts_dir=${work_dir}/scripts_ilogistic
-discard_cycles=10
+set_basename=hivpr
+work_dir=/home/users/egallicchio/Dropbox/src/openmm_sdm_workflow
+bedam_workflow_dir=/home/users/egallicchio/Dropbox/src/bedam_workflow
+scripts_dir=${work_dir}/scripts
+ligands="1 2 3"
+discard_samples=10
 datafilename=repl.cycle.totE.potE.temp.lambda.ebind.lambda1.lambda2.alpha.u0.w0.dat
 
-for lig in 3iodotoluene ; do
+for lig in ${ligands} ; do
 
     jobname=${set_basename}-${lig}
     jobdir=${work_dir}/complexes/${jobname}-ilogistic
@@ -17,25 +19,21 @@ for lig in 3iodotoluene ; do
 	cd r$repl
 	rm -f ${datafilename}
 	n=0
-	for f in `/bin/ls -v ${jobname}_*.out` ; do 
-	    cycle=${f%.out} ; cycle=${cycle#${jobname}_}
-	    if [ $cycle -gt ${discard_cycles} ] ; then 
-		while read -r line ; do
-		    words=(${line// / })
-		    #lambda,lambda1,lambda2,alpha,u0,w0,potE,bindE
-		    # 0       1       2       3   4   5  6     7
- 		    echo "${repl} ${n} ${words[4]} ${words[4]} 300.0 ${words[0]} ${words[7]} ${words[1]} ${words[2]} ${words[3]} ${words[4]} ${words[5]} " >> ${datafilename}
-		    n=`expr $n + 1 `
-		done < $f
-	    fi
-	    
+	for f in `/bin/ls -v ${jobname}*.out` ; do 
+	    while read -r line ; do
+		words=(${line// / })
+		#lambda,lambda1,lambda2,alpha,u0,w0,potE,bindE
+		# 0       1       2       3   4   5  6     7
+ 		echo "${repl} ${n} ${words[4]} ${words[4]} 300.0 ${words[0]} ${words[7]} ${words[1]} ${words[2]} ${words[3]} ${words[4]} ${words[5]} " >> ${datafilename}
+		n=`expr $n + 1 `
+	    done < $f
 	done 
 	cd ..
     done
 
     rm -f result.log Rplots.pdf p-l*.dat
     
-    cat `/bin/ls -v r*/${datafilename}` | awk "\$2 > ${discard_cycles} {print \$0} " > ${datafilename}
+    cat `/bin/ls -v r*/${datafilename}` | awk "\$2 > ${discard_samples} {print \$0} " > ${datafilename}
     
     R CMD BATCH ${scripts_dir}/uwham_analysis.R
     res=`grep 'DGb =' result.log`
